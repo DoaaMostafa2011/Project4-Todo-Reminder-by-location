078fcf7187cd562a7efc16cee55afe066cc3871f
doaa mostafa <eng.doaa2011@yahoo.com>
2022-11-07 15:48:15 +0200

    fix koin dependency
    fix koin dependency in build.gradle

--- app/src/main/java/com/udacity/project4/locationreminders/data/local/RemindersDao.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/data/local/RemindersDao.kt
@@ -16,14 +16,14 @@ interface RemindersDao {
      * @return all reminders.
      */
     @Query("SELECT * FROM reminders")
-     fun getReminders(): List<ReminderDTO>
+    suspend fun getReminders(): List<ReminderDTO>
 
     /**
      * @param reminderId the id of the reminder
      * @return the reminder object with the reminderId
      */
     @Query("SELECT * FROM reminders where entry_id = :reminderId")
-     fun getReminderById(reminderId: String): ReminderDTO?
+    suspend fun getReminderById(reminderId: String): ReminderDTO?
 
     /**
      * Insert a reminder in the database. If the reminder already exists, replace it.
@@ -31,12 +31,12 @@ interface RemindersDao {
      * @param reminder the reminder to be inserted.
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-     fun saveReminder(reminder: ReminderDTO)
+    suspend fun saveReminder(reminder: ReminderDTO)
 
     /**
      * Delete all reminders.
      */
     @Query("DELETE FROM reminders")
-     fun deleteAllReminders()
+    suspend fun deleteAllReminders()
 
 }
